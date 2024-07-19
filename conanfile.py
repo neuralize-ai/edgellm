c63e111749113aefad3fdde17698cf1b4243edd1
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-07-18 23:45:40 -0700

    Add with_gpu and with_npu options, pass along to edgerunner

--- conanfile.py
+++ conanfile.py
@@ -5,12 +5,37 @@ class Recipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
 
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "with_gpu": [True, False],
+        "with_npu": [True, False],
+    }
+
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "with_gpu": True,
+        "with_npu": True,
+    }
+
     def layout(self):
         self.folders.generators = "conan"
 
     def requirements(self):
         self.requires("fmt/10.2.1")
         self.requires("edgerunner/0.1.0")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+        self.options["edgerunner"].with_gpu = self.options.with_gpu
+        self.options["edgerunner"].with_npu = self.options.with_npu
 
     def build_requirements(self):
         self.test_requires("catch2/3.6.0")
