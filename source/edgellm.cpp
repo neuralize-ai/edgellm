a9470f23ba4465f74a9b317feeb3bf77351f9844
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-07-31 11:54:23 -0700

    Add EdgeLLM constructor to store model paths, load Tokenizer and load Sampler

--- source/edgellm.cpp
+++ source/edgellm.cpp
@@ -1,15 +1,23 @@
-#include <string>
+#include <filesystem>
+#include <vector>
 
 #include "edgellm/edgellm.hpp"
 
+#include <edgerunner/edgerunner.hpp>
 #include <fmt/core.h>
+#include <fmt/ranges.h>
 
-exported_class::exported_class()
-    : m_name {fmt::format("{}", "edgellm")}
-{
-}
+namespace edgellm {
 
-auto exported_class::name() const -> char const*
-{
-  return m_name.c_str();
-}
+EdgeLLM::EdgeLLM(std::vector<std::filesystem::path>&& promptProcessorPaths,
+                 std::vector<std::filesystem::path>&& tokenGeneratorPaths,
+                 const std::filesystem::path& tokenizerPath)
+    : m_promptProcessorPaths(std::move(promptProcessorPaths))
+    , m_tokenGeneratorPaths(std::move(tokenGeneratorPaths))
+    , m_creationSuccess(m_tokenizer.load(tokenizerPath))
+    , m_vocabSize(m_tokenizer.getVocabSize())
+    , m_bosId(m_tokenizer.getBosTok())
+    , m_eosId(m_tokenizer.getEosTok())
+    , m_sampler(m_vocabSize, MTemperature, MTopp) {}
+
+}  // namespace edgellm
